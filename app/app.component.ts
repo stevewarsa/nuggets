4dc836b1b944f1e9e4cc3a8056ebc114b74c6abc
Steve Warsa <swarsa480@gmail.com>
2019-07-26 05:43:15 -0700

    Allow navigating to any routing destination without login (as Guest)

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MemoryService } from 'src/app/memory.service';
 import { Subscription } from 'rxjs';
-import { Router, NavigationStart } from '@angular/router';
+import { Router, NavigationStart, NavigationExtras } from '@angular/router';
 import { StringUtils } from 'src/app/string.utils';
 
 @Component({
@@ -24,7 +24,26 @@ export class AppComponent implements OnInit, OnDestroy {
     });
     let dest = StringUtils.getParameterByName('dest');
     if (!StringUtils.isEmpty(dest)) {
-      this.router.navigate([dest]);
+      if (!this.user) {
+        // allow anyone to be sent a link and not have to login
+        this.memoryService.setCurrentUser(this.memoryService.GUEST_USER);
+      }
+      // preserve the query parameters sent in to this route to be passed to next route
+      const url = window.location.href;
+      const params = url.indexOf('?') !== -1 ? url.slice(url.indexOf('?') + 1)
+                  .split('&')
+                  // tslint:disable:no-any
+                  .reduce((params: any, param) => {
+                      const [key, value] = param.split('=');
+                      params[key] = value ? decodeURIComponent(value.replace(/\+/g, ' ')) : '';
+                      return params;
+                  }, {}) : {};
+      let navigationExtras: NavigationExtras = {
+        queryParamsHandling: 'merge',
+        preserveFragment: true,
+        queryParams: params
+      };
+      this.router.navigate([dest], navigationExtras);
     }
   }
   ngOnDestroy() {
