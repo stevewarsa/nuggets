da0c26fb53a3b4aae4854e3446445a33b0d90ae7
Steve Warsa <swarsa480@gmail.com>
2025-05-20 06:46:04 -0700

    Fixed the GoToPassageByRef so that if you click on a valid suggestion, it will go right to it

--- src/components/GoToPassageByRef.tsx
+++ src/components/GoToPassageByRef.tsx
@@ -2,34 +2,40 @@ import React, {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {Button, Col, Container, Form, ListGroup, Row} from "react-bootstrap";
 import {TRANSLATION} from "../models/constants";
-import {getPassageFromPassageRef} from "../models/passage-utils";
+import {getPassageFromPassageRef, getSuggestions} from "../models/passage-utils";
 import {Passage} from "../models/passage";
-import {useSuggestions} from "../hooks/useSuggestions";
 
 const GoToPassageByRef = () => {
     const navigate = useNavigate();
     const [passageRef, setPassageRef] = useState("");
-
-    const suggestions = useSuggestions(passageRef);
+    const [suggestions, setSuggestions] = useState<string[]>([]);
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setPassageRef(e.target.value);
+        setSuggestions(getSuggestions(e.target.value));
     };
 
     const handleSuggestionClick = (suggestion: string) => {
+        const computedSuggestions = getSuggestions(suggestion);
+        if (computedSuggestions.length === 0 || computedSuggestions.includes(suggestion)) {
+            setSuggestions([]);
+            handleGoToPassage(suggestion);
+        } else {
+            setSuggestions(computedSuggestions);
+        }
         setPassageRef(suggestion);
     };
 
-    const handleGoToPassage = () => {
+    const handleGoToPassage = (suggestion?: string) => {
         console.log('Bible Reference:', passageRef);
-        const passagesFromPassageRef: Passage[] = getPassageFromPassageRef(passageRef.trim());
+        const passagesFromPassageRef: Passage[] = suggestion ? getPassageFromPassageRef(suggestion) : getPassageFromPassageRef(passageRef.trim());
         if (passagesFromPassageRef.length > 0) {
             console.log("GoToPassageByRef.handleGoToPassage - Setting the first passage parsed:", passagesFromPassageRef[0]);
             const readChapRoute = `/readBibleChapter/${TRANSLATION}/${passagesFromPassageRef[0].bookName}/${passagesFromPassageRef[0].chapter}/${passagesFromPassageRef[0].startVerse}`;
             console.log("GoToPassageByRef.handleGoToPassage - navigating to route:", readChapRoute);
             navigate(readChapRoute);
         } else {
-            console.log("GoToPassageByRef.handleGoToPassage - no passages parsed from passageRef: " + passageRef.trim());
+            console.log("GoToPassageByRef.handleGoToPassage - no passages parsed from passageRef: " + (suggestion ? suggestion : passageRef));
         }
     };
 
@@ -67,7 +73,7 @@ const GoToPassageByRef = () => {
                     <Button
                         variant="primary"
                         size="lg"
-                        onClick={handleGoToPassage}
+                        onClick={() => handleGoToPassage()}
                     >
                         Go To Passage
                     </Button>
